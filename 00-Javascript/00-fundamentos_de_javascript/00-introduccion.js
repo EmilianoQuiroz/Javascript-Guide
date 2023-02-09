bacf778b430c2fd704c958a177d610383c8a8a71
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-02-09 14:02:29 -0300

    Fundamentos de Javascript

---
+++ 00-Javascript/00-fundamentos_de_javascript/00-introduccion.js
@@ -0,0 +1,61 @@
+/* 
+*███─█──█─███─████─████─████──█─█─████─████─███─████─█──█────████──────██─████─█─█─████─███─████─████─███─████─███
+*─█──██─█──█──█──█─█──█─█──██─█─█─█──█─█──█──█──█──█─██─█────█──█───────█─█──█─█─█─█──█─█───█──█─█──█──█──█──█──█─
+*─█──█─██──█──████─█──█─█──██─█─█─█────█─────█──█──█─█─██────████───────█─████─█─█─████─███─█────████──█──████──█─
+*─█──█──█──█──█─█──█──█─█──██─█─█─█──█─█──█──█──█──█─█──█────█──█────█──█─█──█─███─█──█───█─█──█─█─█───█──█─────█─
+*███─█──█──█──█─█──████─████──███─████─████─███─████─█──█────█──█────████─█──█──█──█──█─███─████─█─█──███─█─────█─
+*/
+
+//**************************************************/
+//*             ¿Que es Javascript?                */
+//**************************************************/
+/*
+JavaScript es un lenguaje de programación que se utiliza principalmente para aportar dinamismo a sitios y aplicaciones web.
+Funciona en complemento con los lenguajes web HTML Y CSS3, permitiendo crear nuevas funcionalidades e interacciones avanzadas con los usuarios, así como interactuar con otras aplicaciones o servicios de backend.
+
+Lenguaje interpretado
+A diferencia de un lenguaje de programación compilado, Javascript es un lenguaje interpretado, lo que significa que se ejecuta por medio de un programa intérprete.
+En nuestro caso, los navegadores son los programas que se encargan de interpretar y ejecutar el código Javascript que creemos. 
+Cuando accedemos a alguna página a través del navegador, éste se encarga de leer y ejecutar todos los archivos que conforman la aplicación (HTML, CSS, JS).
+
+FRONT-END y BACK-END
+Javascript se utiliza tanto para construir aplicaciones de Frontend como de Backend. 
+Por Frontend entendemos a la parte de la aplicación que corre en el navegador y con la cual interactúan los usuarios. 
+Como tal, estaremos creando aplicaciones con Javascript, HTML y CSS; vinculando los tres lenguajes en el desarrollo un único producto.
+Nuestra aplicación de Frontend también consume datos y servicios ofrecidos por algún Backend. Javascript será la herramienta que nos permitirá comunicarnos e intercambiar información con APIs u otras aplicaciones.
+*/
+
+//**************************************************/
+//*            Historia de Javascript              */
+//**************************************************/
+/*
+¿CUÁNDO NACE?
+La primera aparición pública de Javascript la encontramos en el año 1995 cuando se utiliza como herramienta del navegador Netscape Navigator.
+El objetivo fue permitir agregar programas a páginas web.
+
+EVOLUCIÓN DE JAVASCRIPT
+La primera versión de JavaScript ES1 se lanzó en 1997 y el lenguaje fue cambiando con el tiempo. En el curso nos focalizamos en las versiones ES6 y posteriores. Actualmente JS se encuentra en la versión 11 y desarrollo de la 12.
+
+DIFERENCIA CON JAVA
+Java funciona a través de declaraciones que se establecen a partir de un sistema de clases en tiempo de compilación. 
+En cambio, JavaScript se basa en un sistema de tiempo de ejecución donde cada dato distinto puede representar valores booleanos, de cadena o numéricos.
+*/
+
+//**************************************************/
+//*            Sintaxis de Javascript              */
+//**************************************************/
+/*
+Sintaxis: reglas básicas
+Cuando escribimos código JavaScript, hay que tener en cuenta que no importa la cantidad de espacios en blanco que ingresemos, pero tenemos que tener especial cuidado en respetar las mayúsculas y minúsculas al tipear los elementos a utilizar. JavaScript es un lenguaje case sensitive porque distingue entre mayúsculas y minúsculas. Es decir, no es lo mismo escribir “UNO” que “uno”: se consideran diferentes. En comparación, HTML es un lenguaje case insensitive, donde <p> y <P> se entienden iguales.
+Como desarrolladores y al igual que en otros lenguajes, también podemos insertar comentarios en nuestro código. En JS podemos agregar comentarios de una sóla línea con la combinación “//”, o comentarios de bloque, de varias líneas, envolviendo el comentario con los signos /* */
+
+ 	// comentario de una línea
+/*   comentario de
+	     bloque de varias líneas */
+/*
+Sintaxis: palabras reservadas
+Ya sabemos dónde escribimos el código JavaScript, así que ahora vamos a ver cómo lo hacemos. Usamos palabras para escribir las líneas de código, palabras que no determinan el funcionamiento de instrucción (línea de código) que estamos creando. Entonces, para aprender a programar en JavaScript debemos entender el significado de  las palabras reservadas, y cómo podemos emplearlas para programar.
+Algunos ejemplos de estos términos son: break, case, catch, continue, default, let, delete, do, else, finally, for, function, if, in, instanceof, new, return, switch, this, throw, try, typeof, var, void, while, with, etcétera. En el curso aprenderemos a utilizar los más importantes para construir aplicaciones.
+
+*/
+
---
+++ 00-Javascript/00-fundamentos_de_javascript/02-tipos_de_datos.js
@@ -0,0 +1,56 @@
+/*
+*███─███─████─████─███────████──███────████──████─███─████─███────███─█──█──────██─████─█─█─████─███─████─████─███─████─███
+*─█───█──█──█─█──█─█──────█──██─█──────█──██─█──█──█──█──█─█──────█───██─█───────█─█──█─█─█─█──█─█───█──█─█──█──█──█──█──█─
+*─█───█──████─█──█─███────█──██─███────█──██─████──█──█──█─███────███─█─██───────█─████─█─█─████─███─█────████──█──████──█─
+*─█───█──█────█──█───█────█──██─█──────█──██─█──█──█──█──█───█────█───█──█────█──█─█──█─███─█──█───█─█──█─█─█───█──█─────█─
+*─█──███─█────████─███────████──███────████──█──█──█──████─███────███─█──█────████─█──█──█──█──█─███─████─█─█──███─█─────█─
+*/
+
+//Tipos de datos en Javascript
+//-- Tipo de dato String--//
+/* El String es un tipo de dato que contiene caracteres y texto */
+let nombre = "Santiago";
+console.log(nombre); /* En este ejemplo imprimimos un tipo de dato string */
+
+//--Tipo de dato Numerico--//
+let numero = 12;
+console.log(numero); /* En este ejemplo imprimimos el numero de nuestra variable numero */
+
+//--Tipo de dato Boolean--//
+let boolea = true;
+console.log(boolea); /* En este ejemplo imprimimos el valor boolean */
+
+//--Tipo de dato Date--//
+let data = new Date();
+console.log(data); /* En este ejemplo imprimimos el valor date */
+
+//--Tipo de dato Array--//
+let array = [1, 2, 3];
+console.log(array); /* En este ejemplo imprimimos el valor array */
+
+//--Tipo de dato Object--//
+let objeto = {
+    nombre: "Santiago",
+    numero: 12,
+    boolea: true,
+    data: new Date(),
+    array: [1, 2, 3]
+}
+console.log(objeto); /* En este ejemplo imprimimos el objeto
+/* Con un objeto podemos contener varias propiedades */
+
+//-- Tipo de datp Undefined --//
+//Es un tipo de dato que no esta definido
+let x;
+console.log(typeof x);/* Como no hemos guardado ningun valor en la variable x entonces el valor es indefinido */
+
+//-- Tipo de dato Null --//
+//Es un tipo de dato que indica que no tiene ningun valor en la variable
+let y = null;
+console.log(y); /* En este ejemplo imprimimos el valor de y
+/* En javascript las variables son dinamicas, es decir, pueden volver a cambiar su valor */
+
+//-- TypeOf en Javascript --//
+//Con esta propiedad podremos saber que tipos de dato guarda una variable
+
+console.log(typeof nombre);/* Se usa escribiendo typeof seguido del nombre de la variable*/
