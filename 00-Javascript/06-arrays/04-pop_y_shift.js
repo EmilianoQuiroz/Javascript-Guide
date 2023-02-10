c0340cf6cccaf2de135fb1238fdf64c74acf3ced
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-02-10 19:51:04 -0300

    Metodos Pop y Shift en Javascript

---
+++ 00-Javascript/06-arrays/04-pop_y_shift.js
@@ -0,0 +1,22 @@
+/*
+*████─████─████────██─██────███─█──█─███─███─███
+*█──█─█──█─█──█─────███─────█───█──█──█──█────█─
+*████─█──█─████──────█──────███─████──█──███──█─
+*█────█──█─█─────────█────────█─█──█──█──█────█─
+*█────████─█─────────█──────███─█──█─███─█────█─
+*/
+
+//*************************************************/
+//*          Metodo Pop en Javascritp             */
+//*************************************************/
+/*
+De manera inversa a los anteriores, tenemos métodos para eliminar un elemento en el inicio o el final del array. El método pop() elimina el último elemento de un array, no importa cuál fuese, mientras que el método unshift() elimina siempre el primer elemento en el array:
+*/
+
+const nombres = ["Luis", "Ana", "Julia", "Juan"]
+
+nombres.pop()
+console.log(nombres) // ["Luis", "Ana", "Julia"]
+
+nombres.shift()
+console.log(nombres) // ["Ana", "Julia"]
